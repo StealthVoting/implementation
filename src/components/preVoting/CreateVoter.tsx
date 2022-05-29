2460aa70a01444097ae2bb97b8fbd1170ab5030d
parmeet6032 <singh.pmeet752@gmail.com>
2022-05-29 20:55:13 +0530

    feat: basic routing

---
+++ src/components/preVoting/CreateVoter.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './CreateVoter.css';
+
+function CreateVoter() {
+  return (
+    <div className='black-border'>CreateVoter</div>
+  )
+}
+
+export default CreateVoter;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App/>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
